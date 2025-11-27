ea835b82648ace43311067c1c5beb1a59ae4aadd
Li-wd333 <2944413731@qq.com>
2025-11-27 16:29:43 +0800

    空工程

---
+++ web-demo1/web-demo/src/test/java/it/li/WebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.li;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
